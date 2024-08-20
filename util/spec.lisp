21464db4cfa2056082ed2f5d1e8f62d5ae3a02c2
Ryan Gannon <ryanmgannon@gmail.com>
2024-08-19 22:24:59 -0400

    Mon dieu!  A class!

--- util/spec.lisp
+++ util/spec.lisp
@@ -10,6 +10,35 @@
            :service-root-directory))
 (in-package :claws.util.spec)
 
+(defclass aws-service-specification ()
+  ((name :initarg :name
+         :initform (error "You can't have a service without a name.")
+         :type string
+         :reader service-name)
+   (endpoint-rule-set :initarg :endpoint-rule-set
+                      :initform (error "Missing endpoint rule set")
+                      :type hash-table
+                      :reader service-endpoint-rule-set)
+   (examples :initarg :examples
+             :type (or hash-table nil)
+             :reader service-examples)
+   (paginators :initarg :paginators
+               :type (or hash-table nil)
+               :reader service-paginators)
+   (paginators-sdk-extras :initarg :paginators-sdk-extras
+                          :type (or hash-table nil)
+                          :reader service-paginators-sdk-extras)
+   (service-definition :initarg :service-definition
+                       :initform (error "Missing service definition")
+                       :type hash-table
+                       :reader service-definition)
+   (service-sdk-extras :initarg :service-sdk-extras
+                       :type (or hash-table nil)
+                       :reader service-sdk-extras)
+   (waiters :initarg :waiters
+            :type (or hash-table nil)
+            :reader service-waiters)))
+
 (define-condition service-not-found (error)
   ((missing-service :initarg :service
                     :reader service
@@ -65,13 +94,13 @@
       :interactive (lambda () (new-value-input-prompt "Enter a service to locate specs: "))
       (service-root-directory s))))
 
-(defun service-api-directory (service version)
+(defun service-api-directory (service &optional (version (first (service-api-versions service))))
   "Given a SERVICE and VERSION, return a pathname to its specification root"
   (let* ((root-dir (service-root-directory (ensure-string service)))
          (version-dir (merge-pathnames version root-dir)))
     (restart-case
         (if (fad:directory-exists-p version-dir)
-            version-dir
+            (fad:pathname-as-directory version-dir)
             (error (make-condition 'service-api-version-not-found :service service :version version)))
       (pick-new-version (v)
         :report "Enter a valid API version"
@@ -98,11 +127,28 @@
 
 (defun load-service-spec (service &optional (api-version (first (service-api-versions service))))
   "Loads a service spec into a hashmap"
-  (let ((files (service-spec-files service api-version))
-        (spec (make-hash-table :test #'equal)))
-    (dolist (f files)
-      (setf (gethash (file-namestring f) spec) (jzon:parse f)))
-    spec))
+  (let* ((root (service-api-directory service api-version))
+         (definition-file (merge-pathnames "service-2.json" root))
+         (endpoint-rule-file (merge-pathnames "endpoint-rule-set-1.json" root))
+         (examples-file (merge-pathnames "examples-1.json" root))
+         (paginators-file (merge-pathnames "paginators-1.json" root))
+         (paginator-extras-file (merge-pathnames "paginators-1.sdk-extras.json" root))
+         (service-sdk-extras-file (merge-pathnames "service-2.sdk-extras.json" root))
+         (waiters-file (merge-pathnames "waiters-2.json" root)))
+    (make-instance 'aws-service-specification
+                   :name (ensure-string service)
+                   :service-definition (jzon:parse definition-file)
+                   :endpoint-rule-set (jzon:parse endpoint-rule-file)
+                   :examples (when (uiop:file-exists-p examples-file)
+                               (jzon:parse examples-file))
+                   :waiters (when (uiop:file-exists-p waiters-file)
+                              (jzon:parse waiters-file))
+                   :service-sdk-extras (when (uiop:file-exists-p service-sdk-extras-file)
+                                         (jzon:parse service-sdk-extras-file))
+                   :paginators (when (uiop:file-exists-p paginators-file)
+                                 (jzon:parse paginators-file))
+                   :paginators-sdk-extras (when (uiop:file-exists-p paginator-extras-file)
+                                            (jzon:parse paginator-extras-file)))))
 
 (defun all-service-names ()
   (subdirectory-names (asdf:system-relative-pathname :claws "data/")))
